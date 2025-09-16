bc55ab44ef24d3dedd411b7561c5d9cd703d3b67
snowdy78 <themrleoner12@mail.ru>
2025-09-16 10:27:49 +0300

    basic configuration

--- NouApp/src/main.jsx
+++ NouApp/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom'
+import { StrictMode } from 'react'
+
+
+/** 
+ * Base Application Component 
+ */
+export function AppComponent() {
+  return (
+    <StrictMode>
+      <StoreProvider>
+        <BrowserRouter>
+          <Routes>
+            
+          </Routes>
+        </BrowserRouter>
+      </StoreProvider>
+    </StrictMode>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <AppComponent/>,
 )
