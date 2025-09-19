fce5bc5d4bf0c04ce09a0d949221ab71177375cf
snowdy78 <themrleoner12@mail.ru>
2025-09-20 05:26:00 +0700

    creating a channel

--- NouApp/src/components/Channels.jsx
+++ NouApp/src/components/Channels.jsx
@@ -2,29 +2,29 @@ import React from 'react';
 import { useStore } from '../hooks/useStore';
 import './css/Channels.css';
 import { useWebSocket } from '../hooks/useWebSocket';
+import {Link} from 'react-router-dom';
 
 export function Channels() {
     const store = useStore();
-    const user = React.useRef(null);
     const [channelLoadWarning, setChannelLoadWarning] = React.useState(null);
+    const [channels, setChannels] = React.useState([]);
+    const webSocketActions = {
+        'userchannels': onUserChannels,
+        'channel': onChannel,
+        'message': onMessage,
+    };
+
     const ws = useWebSocket(() => {
         ws.current.onopen = () => {
+            ws.current.onmessage = (messageEvent) => {
+                const messageData = JSON.parse(messageEvent.data);
+                if (messageData && messageData.type && webSocketActions[messageData.type]) {
+                    console.log(messageData.type);
+                    webSocketActions[messageData.type](messageData);
+                }
+            };
             if (store.user.channels > 0) {
-                ws.current.onmessage = (messageEvent) => {
-                    const messageData = JSON.parse(messageEvent.data);
-                    if (messageData.type !== 'userchannels') {
-                        return;
-                    }
-                    if (messageData.username !== store.user.name) {
-                        return;
-                    }
-                    if (messageData.warn) {
-                        setChannelLoadWarning('Not all channels are load.');
-                        return;
-                    }
-
-                };
-                ws.current.send(JSON.stringify({ type: 'userchannels', username: user.current.name }));
+                ws.current.send(JSON.stringify({ type: 'userchannels', username: store.user.name }));
             }
         };
     });
@@ -32,30 +32,76 @@ export function Channels() {
         if (!store.user) {
             window.location = '/';
         }
-        user.current = store.user;
-    }, [user, store]);
+    }, [store.user]);
 
+    function onChannel(data) {
+        if (data.username !== store.user.name) {
+            return;
+        }
+        const channel = data.data;
+        store.user.addChannel(channel.name);
+        setChannels([...channels, {name: channel.name, lastMessage: channel.messages[channel.messages.length - 1]}]);
+    }
+    function onUserChannels(data) {
+        if (data.username !== store.user.name) {
+            return;
+        }
+        if (data.warn) {
+            setChannelLoadWarning('Not all channels are load.');
+            return;
+        }
+        store.user.addChannel(data.channelName);
+        setChannels([...channels, ...data.data.channels]);
+    }
+    function onMessage(data) {
+        if (data.username !== store.user.name) {
+            return;
+        }
+        const channelIndex = channels.findIndex(data.channelName);
+        if (channelIndex === -1) {
+            return;
+        }
+        channels[channelIndex].lastMessage = data.data;
+        setChannels([...channels]);
+    }
+    function onChannelAdd(event) {
+        event.preventDefault();
+        const channel = document.querySelector('.channels-component__input-channel-name');
+        if (channel) {
+            ws.current.send(JSON.stringify({type: 'channel', username: store.user.name, channelName: channel.value }));
+            channel.value = '';
+        }
+    }
     let warnComponent;
     if (channelLoadWarning) {
         warnComponent = <div className='warn-field'>{channelLoadWarning}</div>;
     }
     return (
         <div className='channels-component'>
-            <div className='header'>
-
+            <div className='channels-component__header'>
+                {
+                <form onSubmit={onChannelAdd} style={{display: 'flex', width: '100%', gap: 5}}>
+                    <input type="text" className='channels-component__input-channel-name' placeholder='Enter channel name to connect or create'/>
+                    <button type="submit">&gt;</button>
+                </form>
+                }
+                
             </div>
             <div className="channel-list">
                 {warnComponent}
-                {user.current.channels.length === 0 
+                {channels.length === 0
                 ? 
                 <div className='hidden-text'>
                     Channels does not exist
                 </div> 
                 : 
-                user.current.channels.map((value, index) => (
+                channels.map((value, index) => (
                     <div key={`channel-list__channel${index}`} className="channel-list__channel">
-                        <div className="channel-list__channel_Name">
-
+                        <div className="channel-list__channel__name">
+                            <Link to={`/channel/${value.name}`}>{value.name}</Link>
+                        </div>                        
+                        <div className="channel-list__channel__last-message">
+                            {value.lastMessage ? `${value.lastMessage.authorName}:${value.lastMessage.text}` : 'No messages sended'} 
                         </div>
                     </div>
                 ))}
--- NouApp/src/components/SessionCreate.jsx
+++ NouApp/src/components/SessionCreate.jsx
@@ -5,27 +5,39 @@ import { useStore } from '../hooks/useStore';
 import { useWebSocket } from '../hooks/useWebSocket';
 
 export function SessionCreate() {
-    const ws = useWebSocket();
-    const store = useStore();
-    function onSubmit(e) {
-        e.preventDefault();
-        const username = document.querySelector('input').value;
+    const ws = useWebSocket(() => {
         ws.current.onmessage = (messageEvent) => {
             const messageData = JSON.parse(messageEvent.data);
             if (messageData.type !== 'user') {
                 return;
             }
-            if (!messageData.error && messageData.data) {
-                console.log(messageData.data);
+            if (messageData.error) {
+                setLoginError(messageData.error.message);
+            }
+            else if (messageData.data) {
                 store.initUser(messageData.data.name, messageData.data.channels);
                 sessionStorage.setItem('user', JSON.stringify(messageData.data));
                 window.location = '/channels';
+                return;
+            }
+            else {
+                console.error('No response message data.');
             }
         };
+    });
+    const store = useStore();
+    const [loginError, setLoginError] = React.useState(null);
+    function onSubmit(e) {
+        e.preventDefault();
+        const username = document.querySelector('input').value;
+        
         ws.current.send(JSON.stringify({type: 'user', username}));
     }
     return (
         <div>
+            { loginError ?
+            <div className='error-field'>{loginError}</div> : null
+            }
             <form className='login-form' action="" onSubmit={onSubmit}>
                 <input type="text" className='login-form__user-name-input' placeholder="/* Enter name */"/>
                 <button className='login-form__submit-button'>/Chat</button>
